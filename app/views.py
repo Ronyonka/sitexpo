e27968867a34499e02f979eb24221916f5dd9488
Ronyonka <rononyonka@gmail.com>
2019-03-17 12:47:47 +0300

    added signup

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
+
+def signup(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenocate(usernamme=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html',{'form':form})
 
 def home(request):
     return render(request, 'home.html')
