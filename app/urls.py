1d49378454447293cbe852651c4821cde540b2f6
Ronyonka <rononyonka@gmail.com>
2019-03-17 12:58:07 +0300

    added templates

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,14 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
 
 urlpatterns=[
-    url('^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name':'login.html'},name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
