4c8fdff5d58a5adbd43bb404452f06941046b436
Ronyonka <rononyonka@gmail.com>
2019-03-18 14:38:57 +0300

    debugged commenting error

--- project/settings.py
+++ project/settings.py
@@ -148,6 +148,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+LOGIN_REDIRECT_URL="home"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
