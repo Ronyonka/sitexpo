c81cfbc4a6717df1f43120f4470499513088c384
Ronyonka <rononyonka@gmail.com>
2019-03-18 11:04:48 +0300

    added profile.html

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'', include('app.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    # url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^tinymce/',include('tinymce.urls')),
 ]
