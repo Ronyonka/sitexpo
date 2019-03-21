eaa2cc25f81293384bca2c4d4c884c238e6b66af
Ronyonka <rononyonka@gmail.com>
2019-03-21 09:34:14 +0300

    changed some errors

--- app/views.py
+++ app/views.py
@@ -93,7 +93,7 @@ def edit_profile(request):
          profile.user = user
          profile.save()
          messages.info(request, 'You\'ve successfully updated your account!')
-         return redirect('home')
+         return redirect('edit_profile')
    else:
       form = ProfileUpdateForm(instance=request.user)
       user_form = UserUpdateForm(instance=request.user.profile)
