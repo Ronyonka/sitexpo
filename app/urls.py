f9cb2488ed4e15e0b85ed9d50f1824c1c245a611
Ronyonka <rononyonka@gmail.com>
2019-03-21 09:28:21 +0300

    changed some errors

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,7 @@ urlpatterns=[
     url(r'login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
-    url(r'profile/edit', views.edit_profile, name='edit_profile'),
+    url(r'^edit/$', views.edit_profile, name='edit_profile'),
     url(r'^search/', views.search, name='search'),
     url(r'^api/profiles/$',views.ProfileList.as_view()),
     url(r'^api/projects/$',views.ProjectList.as_view())
